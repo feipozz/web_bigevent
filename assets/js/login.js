edd0074605a6980cd0ad0cb0de6bf799e7844e32
boring_kk <278366950@qq.com>
2020-09-29 20:24:43 +0800

    完成了登录和注册的功能开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -42,7 +42,7 @@ $(function () {
       password: $("#form_reg [name=password]").val(),
     };
     //2.发起ajax 的post请求
-    $.post("http://ajax.frontend.itheima.net/api/reguser", data, function (
+    $.post("/api/reguser", data, function (
       res
     ) {
       if (res.status !== 0) {
@@ -53,6 +53,28 @@ $(function () {
       layer.msg("注册成功,请登录");
       // 模拟人的点击行为
       $("#link_login").click();
+    });
+  });
+
+  //监听登录表单的提交事件
+  $("#form_login").submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      url: "/api/login",
+      method: "post",
+      //快速获取表单中的数据
+      data: $(this).serialize(),
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg("登录失败！");
+        }
+        layer.msg("登录成功！");
+          // 将登录成功得到的 token 字符串，保存到 localStorage 中
+        localStorage.setItem('token',res.token)
+        
+        //跳转到后台主页
+           location.href='/index.html'
+      },
     });
   });
 });
